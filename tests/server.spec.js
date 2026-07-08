c6cf68ea854db24a6189235b70bde60c00c757a6
amauric3 <mauriciomarines96@gmail.com>
2026-07-07 23:09:35 -0600

    Intentionally break test

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -43,7 +43,7 @@ describe('server/app.js', function() {
       .get('/')
       .end((err, res) => {
         expect(err).not.exist;
-        expect(JSON.stringify(res.text)).to.contain('Deliver It Well');
+        expect(JSON.stringify(res.text)).to.contain('Broken Test');
         done();
       });
   });
